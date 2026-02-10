85cd562ca21d38b0e94729c9943b9c5cd3b4cf6b
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 04:51:12 +0000

    .

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *main-njfsjknfn
+ *Return:ajkfahf
+ *dhiodhikhifikhikhi
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar('\n');
+}
+return (0);
+}
