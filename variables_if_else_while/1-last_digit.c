1104f84b2255a96d739ec8b2f9bdcd961984773c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 02:54:56 +0000

    .

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,13 +11,13 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is ", n);
-if (n > 5)
+if (n % 10 > 5)
 {
   printf("%d and is greater than 5\n", n % 10);
 }
 else if (n == 0)
 {
-printf("%d and is 0\n", n % 10);
+printf("%d and is 0\n", n);
 }
 else if (n < 6)
 {
