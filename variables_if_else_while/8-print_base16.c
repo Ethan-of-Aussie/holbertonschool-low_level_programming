d5164f700df2a549e1ff3282a01231f17c0b087c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 09:34:10 +0000

    .

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-kjjdijajidj
+ *Return:dhsiuhahd
+ *fjjjjjjjjjjjjjjjjjjjjjjjjjjs
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 9; c++)
+{
+putchar('0' + c);
+}
+for (c = 0; c < 6; c++)
+{
+putchar('0' + c);
+}
+putchar('\n');
+return (0);
+}
