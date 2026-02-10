21044384ccb9b8f817c952464259799d46260735
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 08:20:21 +0000

    .

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-kjjdijajidj
+ *Return:dhsiuhahd
+ *fjjjjjjjjjjjjjjjjjjjjjjjjjjs
+ */
+int main(void)
+{
+int count;
+for (count = 0; count <= 10; count++)
+{
+putchar(count);
+}
+putchar('\n'); 
+return (0);
+}
