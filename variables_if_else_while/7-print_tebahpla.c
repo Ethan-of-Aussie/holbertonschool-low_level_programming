ede281f7965f8fe52dfa5a91ebd01d6dd8c14d6a
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 09:02:42 +0000

    .

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main-njfsjknfn
+ *Return:ajkfahf
+ *dhiodhikhifikhikhi
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
