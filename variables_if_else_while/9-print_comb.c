a61c3e96bea8e6053b06285f1e8ff9ffc1d4657d
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 10:16:30 +0000

    .

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main-kjjdijajidj
+ *Return:dhsiuhahd
+ *fjjjjjjjjjjjjjjjjjjjjjjjjjjs
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+putchar('0' + c);
+if (c < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
