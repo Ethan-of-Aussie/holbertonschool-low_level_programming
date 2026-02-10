3869ad6f32cfc7ce38548288e57bffabf63f2b4c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 01:43:45 +0000

    .

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/* more headers goes there */
-/* betty style doc for function main goes there */
-
+/**
+ *main-fhjs
+ *Return:ja8ihdah
+ */
 int main(void)
 {
 int n;
