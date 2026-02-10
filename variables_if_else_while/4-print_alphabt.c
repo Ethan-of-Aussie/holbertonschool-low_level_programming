595015950d6e48bd7317a1cad94c3809f986f383
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-10 07:10:36 +0000

    .

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-kjjdijajidj
+ *return:dhsiuhahd
+ */
+int main(void)
+{
+char lul;
+for (lul = 'a'; lul <= 'z'; lul++)
+{
+for (lul = 'q'; 'q' == 0; lul++)
+for (lul = 'e'; 'e' == 0; lul++)
+{
+putchar(lul);
+}
+}
+return (0);
+}
