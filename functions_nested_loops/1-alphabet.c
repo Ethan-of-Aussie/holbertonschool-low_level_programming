73999ba565d1ee76b7fb4042ac04aad5ef37fa40
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-14 07:58:43 +0000

    .

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main-check the code
+ * Return:0
  */
 void print_alphabet(void)
 {
 	char a = 'a';
-	while (a <= 'z') {
+
+	while (a <= 'z')
+{
 	_putchar(a);
-	a++;}
-	_putchar('\n'); 
+	a++;
+	_putchar('\n'); }
 }
