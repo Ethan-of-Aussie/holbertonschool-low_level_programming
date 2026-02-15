d159fcd57cdfb26ba8049c8bda1dd96de83a0c8e
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-15 01:35:20 +0000

    .

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,24 @@
 #include "main.h"
 #include <unistd.h>
 /**
- *main-temp
+ *print_alphabet_x10 - entry point
+ *
  *Return: void
+ *
  *description: DOING IT BY TEN!!!!
  */
+
 void print_alphabet_x10(void)
 {
 	int i = 1;
-	while (i <= 10)
+
+	while
+	  (i <= 10)
 {
 	char a = 'a';
-	while (a <= 'z')
+
+	while
+	  (a <= 'z')
 {
 	_putchar(a);
 	a++;
