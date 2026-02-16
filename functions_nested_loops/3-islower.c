4dfd1a01c5afb4561516f924b6f04f5c99d3306b
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-16 12:28:48 +0000

    .

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,19 +1,20 @@
 #include <unistd.h>
 #include "main.h"
 /**
- *int _islower - entry point
- *return: 
- *description - weed out the none lowercase.
+ *_islower - Entry point
+ *Return: c as 0 and 1 depending on lowercase c or not
+ *@c: hopefully responds to 3-main.c or otherwise appear as 011
+ *description: 'weed out none lowercase and appear them as 0 while c is 1'
  */
 int _islower(int c)
 {
-  if ((c) >= 'A' && (c) <= 'Z')
+	if ((c) >= 'A' && (c) <= 'Z')
 {
-	c=0;
+	c = 0;
 }
 	if ((c)  >= 'a' && (c) <= 'z')
 {
-	c=1;
+	c = 1;
 }
 	return (c);
 }
