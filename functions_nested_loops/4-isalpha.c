ea7f962822c56f6415fac308b9bb539187320520
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-16 12:58:06 +0000

    .

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - check the code.
- *
+ *_isalpha - check the code.
+ *Desrciption: 'function that checks for alphabetic character'
+ *@c:returns 1 if c is a letter, lowercase or uppercase Returns 0 otherwise
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-        if ((c) >= 'A' && (c) <= 'z')
+	if ((c) >= 'A' && (c) <= 'z')
 {
-        c = 1;
+	c = 1;
 }
-        if ((c)  >= '!' && (c) <= '@')
+	if ((c)  >= '!' && (c) <= '@')
 {
-        c = 0;
+	c = 0;
 }
-        return (c);
+	return (c);
 }
