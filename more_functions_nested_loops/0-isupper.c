d95eea9a3ed4c2e76010062d86bf37699975bef8
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-18 05:42:40 +0000

    .

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ *description:'to Make sure the input is a Captial letter and return 1'
+ *@c: input.
+ *Return: input and number return.
+ */
+int _isupper(int c)
+{
+	if ((c) >= 'A' && (c) <= 'Z')
+	{
+	return (1);
+	return (c);
+	}
+	else
+	{
+	return (0);
+	return (c);
+	}
+}
