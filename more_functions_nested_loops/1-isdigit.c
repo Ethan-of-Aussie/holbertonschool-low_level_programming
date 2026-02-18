2ef2a0b19e8317bfd4ee23272037bbe62b65668d
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-18 05:54:04 +0000

    .

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isdigit - entry point
+ *description: 'To find through 0 - 9 if correct, an number'
+ *@c: input
+ *Return: input and number return
+ */
+int _isdigit(int c)
+{
+	if ((c) >= 0 && (c) <= 9)
+	{
+	return (1);
+	return (c);
+	}
+	else
+	{
+	return (0);
+	return (c);
+}
+}
