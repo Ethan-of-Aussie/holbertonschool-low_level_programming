059b5ed87b8b13c73178809f55761756b9b51202
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-18 06:32:47 +0000

    .

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,12 +2,15 @@
 /**
  *mul - entry
  *description: 'militiply the ints inputs'
- *
- *
+ *@a: first input
+ *@b: second input
+ *Return: input and the 0
 */
 int mul(int a, int b)
 {
 	int t = a * b;
+
 	return (t);
+
 	return (0);
 }
