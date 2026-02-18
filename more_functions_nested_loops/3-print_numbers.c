40c946fc50af05a8a0fd33409e685480ceda5239
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-18 06:50:08 +0000

    .

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - entry
+ *description: ' 0 to 9, print out those numbers'
+ *
+ */
+void print_numbers(void)
+{
+	int p;
+	for (p = 0; p <= 9; p++)
+	  {	
+	    _putchar(p + '0');
+	  }
+_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_numbers(void);
 int _isdigit(int c);
 int _putchar(char c);
 int mul(int a, int b);
