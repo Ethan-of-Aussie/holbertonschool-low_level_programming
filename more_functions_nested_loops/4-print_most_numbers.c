7d85065c09f4842b5b464741712cd3bb3e343f85
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-18 07:20:32 +0000

    .

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_most_numbers - entry
+ *discription: ' DISAPPEAR 2 AND 4 FROM 0 TO 9!'
+ */
+void print_most_numbers(void)
+{
+ int a;
+ for (a = 0; a <= 9; a++)
+   if (a != 2 && a != 4)
+     {
+       _putchar(a + '0');
+     }
+ _putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_most_numbers(void);
 void print_numbers(void);
 int _isdigit(int c);
 int _putchar(char c);
