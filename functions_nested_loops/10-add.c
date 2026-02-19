aa7507819e805212a1576941f66b501603b42eaa
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-19 08:08:31 +0000

    .

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - entry point
+ *Description: 'make the sum of both c and b'
+ *@c: first number
+ *@b: second number
+ *Return: the sum
+*/
+int add(int c, int b)
+{
+	int s;
+
+	s = c + b;
+	return (s);
+}
