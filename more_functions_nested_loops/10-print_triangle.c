3147d86d8386373db6a7c17773ba5513edd3bb18
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-20 05:45:40 +0000

    .

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+print_triangle - entry
+*description: 'function that prints a triangle'
+*@size: input
+*/
+void print_triangle(int size)
+{
+	int tail;
+	int fol;
+	int spa;
+	
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	for (tail = 0; tail < size; tail++)
+	{
+	for (spa = size - tail - 1; spa > 0; spa--)
+	{
+	_putchar(' ');
+	}
+	for (fol = 0; fol <= tail; fol++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+}
