c801e3b87987b3931a6526589d093e20469487de
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-20 23:46:13 +0000

    .

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *swap_int - entry
+ *description: 'function that swaps the values of two integers'
+ *@a: input
+ *@b: input
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
