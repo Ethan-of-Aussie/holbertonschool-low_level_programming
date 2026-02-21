0ab0780036c98c9a6f808985fb86db7128cd9506
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-21 03:56:30 +0000

    .

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *puts_half - entry
+ *description: 'function that prints half of a string'
+ *@str: input
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int sta;
+
+	while (str[len] != '\0')
+	{
+	len++;
+	sta = (len + 1) / 2;
+	}
+	while (str[sta] != '\0')
+	{
+	_putchar(str[sta]);
+	sta++;
+	}
+	_putchar('\n');
+}
