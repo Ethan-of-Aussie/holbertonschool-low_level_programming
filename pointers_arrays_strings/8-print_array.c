5403baadf48e6763623c15ebeb1151d2386d0be6
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-21 04:39:01 +0000

    .

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - entry
+ *description: ' function that prints number elements of an array of integers'
+ *@a: first input
+ *@n: second input
+ */
+void print_array(int *a, int n)
+{
+	int len = 0;
+	
+	while (len < n)
+	{
+	printf("%d", a[len]);
+	len++;
+	if (len < n)
+	printf(", ");
+	}
+printf("\n");
+}
