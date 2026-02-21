71b7c5de595339d0a007ebd048ac08e62e962570
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-21 02:52:21 +0000

    .

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *puts2 - entry
+ *description: 'function that prints every other character of a string'
+ *@str: input
+*/
+void puts2(char *str)
+{
+	int s = 0;
+
+	while (str[s] != '\0')
+	{
+	_putchar(str[s]);
+	s += 2;
+	}
+	_putchar('\n');
+}
