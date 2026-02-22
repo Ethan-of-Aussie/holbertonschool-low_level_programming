5bb1ead27bf86efbb2c41fdf524b5788aa07a44c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-22 00:27:44 +0000

    .

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strcpy - entry
+ *description: 'function that copies the string'
+ *@dest: first input
+ *@src: second input
+ *Return: Returns the copied elements in dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+	dest[i] = src[i];
+	i++;
+	}
+	return (dest);
+}
