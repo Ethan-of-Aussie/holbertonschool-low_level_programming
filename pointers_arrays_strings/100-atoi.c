0b1adba97d8bc2d7331d6b3360c3cac8022bf793
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-22 02:49:30 +0000

    .

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_atoi - entry
+ *description: 'function that convert a string to an integer'
+ *@s: input
+ *Return: the number and its possible left over sign
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 0;
+	int signs = 1;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+	if (s[i] == '-')
+	signs = -signs;
+	i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+	n = n * 10 + (s[i] - '0');
+	i++;
+	}
+	return (n * signs);
+}
