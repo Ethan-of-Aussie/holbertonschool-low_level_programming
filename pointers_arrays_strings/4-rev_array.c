3917f613fb431278c8b4c4f9b032203255ece74c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-23 06:09:04 +0000

    .

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *reverse_array - entry
+ *description: 'reverse the array'
+ *@a: array input
+ *@n: array output
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int r = n - 1;
+	int temp;
+	
+	while (i <= r)
+	{
+		temp = a[i];
+		a[i] = a[r];
+		a[r] = temp;
+		r--;
+		i++;
+		  }
+
+}
