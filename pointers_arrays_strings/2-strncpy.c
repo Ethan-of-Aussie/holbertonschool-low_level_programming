ba630bf2aef50bbc3e90b112e296d8990a4a7ca6
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-23 04:45:10 +0000

    .

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ *_strncpy - entry
+*description: 'copies strings, uses n's bytes from src'
+*@dest: where the copied elements go
+*@src: elements to be copied
+*@n: bytes of numbers
+*Return: the inputs output
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a = 0;
