f168b60b9bc5e662f8b5cd68c9f2f28ed27368a8
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-23 02:05:53 +0000

    .

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,8 @@ int _atoi(char *s)
 	int i = 0;
 	int n = 0;
 	int signs = 1;
-
+	int digit;
+	
 	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 	if (s[i] == '-')
@@ -19,10 +20,12 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-	n = n * 10 + (s[i] - '0');
+	digit = (s[i] - '0');
+	if (n < (-2147483648 + digit) / 10)
+	return (-2147483648);
+	n = n * 10 - digit;
 	i++;
-	if (n <= -2147483648)
-	  n = -2147483648;
+	
 	}
-	return (n * signs);
+	return (signs == 1) ? -n : n;
 }
