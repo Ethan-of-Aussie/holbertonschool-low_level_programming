842142c714f8fca8e528f5d073172568a5d2077c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-23 23:51:45 +0000

    .

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,9 +1,15 @@
 #include "main.h"
-
+/**
+ *_memset - entry
+ *description: 'fills memory with constant bytes'
+ *@s: memory
+ *@b: constant byte
+ *@n: fills the first bytes of the memory
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-  
+
 	for (i = 0; i < n; i++)
 	{
 	((unsigned char *) s)[i] = b;
