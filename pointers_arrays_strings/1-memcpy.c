aa7372db1090f561b510d737dc8afbd6d3018076
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-24 00:36:47 +0000

    .

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -5,7 +5,8 @@
  *@dest: output
  *@src: items to be copied
  *@n: bytes from memory area src
- */
+ *Return: the output 
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
