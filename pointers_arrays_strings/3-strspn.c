87ae5b39b09487d4a0c615af35cf5cbcc4b9c2db
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-02-25 01:36:01 +0000

    .

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,28 @@
 #include "main.h"
-
+/**
+ *_strspn - entry
+ *description: 'return initial segment of byte of s of byte from accept'
+ *@s: intial segment of bytes
+ *@accept: bytes that s will consist of
+ *Return: the output
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len = 0;
+	unsigned int len = 0, j;
 
-	while (len < accept && s[len] != '\0')
+	while (s[len] != '\0')
 	{
-	s[len] = accept[len];
-	len++;
+	j = 0;
+		while (accept[j] != '\0')
+			{
+			if (s[len] == accept[j])
+			break;    
+			j++;
+			}
+		if (accept[j] == '\0')
+		return (len);
+		len++;
 	}
-	return (s);
+	
+	return (len);
 }
