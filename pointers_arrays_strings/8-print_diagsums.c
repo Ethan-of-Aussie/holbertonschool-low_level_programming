43c8082c30bbbe0600bacc35113b02f98a12b251
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-01 03:54:31 +0000

    .

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *
  *
  */
 void print_diagsums(int *a, int size)
 {
-
+	int i, j;
+	
+	for (i = 0; i < *a; i++)
+	{
+	for (j = 0; j < *a; j++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+	}
 }
