b349a1823e85472e359694bb8d578ad19113921b
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-01 06:17:43 +0000

    .

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_diagrams - sum of two diagonals
+ *print_diagsums - sum of two diagonals
  *@a: points to first element
  *@size: number of rows and columns
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 1;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
