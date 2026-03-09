67d11b1e687456e3065979aff232c15b2bd568ed
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-09 11:33:02 +0000

    .

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -26,7 +26,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (nlen = 0; name[nlen] != '\0'; nlen++)
 		;
 	d->name = malloc(nlen + 1);
-	if(!d->name)
+	if (!d->name)
 	{
 		free(d);
 		return (NULL);
