3ee1a19513596cb890036f9dd9e369943ec737ef
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-13 04:13:37 +0000

    .

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,1 +1,20 @@
-int (*get_op_func(char *s))(int, int);
+#include "3-calc.h"
+op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
+int i;
+/**
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+  if (!s)
+    return (NULL);
+  while
+    
+}
