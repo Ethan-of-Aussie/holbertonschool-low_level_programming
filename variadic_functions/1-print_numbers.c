27dd7de37f3b9783c74b6fc082990728aba1a4e1
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-14 05:30:45 +0000

    .

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - print numbers from parameters 
+ *
+ *@separator: Takes on ',' from parameter
+ *@n: Numbers in parameter
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+	int val;
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		{
+		val = va_arg(ap, int);
+		printf("%d", val);
+			if (separator && i < n - 1)
+			printf("%s", separator);
+		}
+	printf("\n");
+	va_end(ap);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdarg.h>
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 #endif
