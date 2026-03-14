f857be7b174d5d448f678c27d8bb9f93fa13126c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-14 06:41:09 +0000

    .

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,21 +1,29 @@
 #include "variadic_functions.h"
 /**
+ *print_strings - I'll be Printing strings
  *
+ *@separator: Takes on ','
+ *@n: Takes on the string
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
   unsigned int i;
 
 	va_list ap;
-	int str;
+	char *str;
 	
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-	  {
-	    str = va_arg(ap, int);
-	    
-	  }
-	va_end();
+	{
+	str = va_arg(ap, char *);
+		if (!str)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
 	printf("\n");
 }
