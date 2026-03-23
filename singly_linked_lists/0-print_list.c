6e810554fa9e2d857e09693610dd4819c900756e
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-23 08:17:17 +0000

    .

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 /**
+ *print_list - prints elements of list_t
+ *@h: pointer to nodes in list_t
  *
- *
+ *Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	list_t *node = malloc(sizeof(list_t));
-	int i = 0;
+	size_t count = 0;
 
+	while (h)
+	{
 	if (!h->str)
-	  {
-	    printf("[0] (nil)\n");
-	  }
+		printf("[0] (nil)\n");
 	else
 	  printf("[%u] %s\n", h->len, h->str);
-	for (i = 0; len[i] < h; i++)
-	  ;
-	
-	printf("[%d]", );
-	return ();
+	h = h->next;
+	count++;
+	}
+	return (count);
 }
