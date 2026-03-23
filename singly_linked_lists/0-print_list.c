4e23dfa314ed9f12b24757d57dfe55e455ccea7e
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-23 07:50:52 +0000

    .

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,8 +6,15 @@
 size_t print_list(const list_t *h)
 {
 	list_t *node = malloc(sizeof(list_t));
-	int num = 0;
+	int i = 0;
 
+	if (!h->str)
+	  {
+	    printf("[0] (nil)\n");
+	  }
+	for (i = 0; len[i] < h; i++)
+	  ;
+	
 	printf("[%d]", );
-	return (num);
+	return ();
 }
