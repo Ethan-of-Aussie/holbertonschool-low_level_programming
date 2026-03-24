00845fb83ae4412210fcfd1fdeded777c0650bd5
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-24 00:47:50 +0000

    .

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ *add_node_end - adds a new node at the end of a list_t
+ *@head: pointer to the pointer to the head of the list
+ *@str: string to be duped into new node
+ *
+ *Return: the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int len = 0;
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *current;
+
+	if (!new_node)
+		return (NULL);
+	while (str[len])
+		len++;
+
+	new_node->len = len;
+	new_node->str = strdup(str);
+
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	current = *head;
+	while (!current->next)
+		current = current->next;
+	current->next = new_node;
+
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
