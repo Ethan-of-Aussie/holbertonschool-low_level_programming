ef0404782474750c3c9cf5d60380dec2e8fbf32a
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-25 07:06:27 +0000

    .

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,3 +3,21 @@
  *
  */
 size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t* head;
+	dlistint_t* tail;
+	dlistint_t* n;
+
+newlist = malloc(sizeof(dlistint_t));
+	if (!newlist)
+	return(NULL);
+
+  n = new dlistint_t;
+  n->next = 1;
+  n->prev = NULL;
+  head = n;
+  tail = n;
+
+  while (h)
+    
+}
