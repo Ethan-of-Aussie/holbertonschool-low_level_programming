b59624d9e6277f6f38b6c11d32bfb7760489e5c6
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-27 01:26:27 +0000

    .

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -3,3 +3,17 @@
  *
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode = malloc(sizeof(*newnode));
+	if (!newnode)
+	return (NULL);
+
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next = *head;
+
+	if (*head != NULL)
+	(*head)->prev = newnode;
+	*head = newnode;
+return (newnode);
+}
