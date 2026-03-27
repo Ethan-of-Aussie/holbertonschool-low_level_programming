63b1d27d0c2a5603eee127ee9798d564d7a56484
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-27 01:29:19 +0000

    .

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 /**
+ *add_dnodeint - Apply newnode to the beginning of list
  *
+ *@head: The head, to apply newnode at beginning of list
+ *@n: The value to send into newnode
+ *Return: The value in newnode at beginning of list
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode = malloc(sizeof(*newnode));
+
 	if (!newnode)
 	return (NULL);
 
