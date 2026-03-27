7b082c2838be3f08d06e5ea3f42d81059cf8e6a1
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-27 00:22:24 +0000

    .

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,18 @@
 #include "lists.h"
 /**
+ *dlistint_len - Return length of linked list
  *
+ *@h: The pointer to struct head
+ *Return: The length
  */
 size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
