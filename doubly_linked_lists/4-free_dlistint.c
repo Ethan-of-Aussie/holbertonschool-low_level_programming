4664f0695c315c226bf3bb1c6a25e691e343e6ab
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-27 06:10:36 +0000

    .

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ *free_dlistint_t - time to go free
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL){
+	temp = head->next;
+	free (head);
+	head = temp;
+	}
+}
