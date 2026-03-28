2c7c4fcea2a7fa8fc5a8fac5798b4effcac112a4
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-28 05:25:55 +0000

    .

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,5 +4,21 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	if (!(*head))
+	{
+		(*head) = malloc(sizeof(dlistint_t));
+		(*head)->n = index;
+		(*head)->next = NULL;
+		(*head)->prev = NULL;
+		return;
+	}
 
+	dlistint_t *newmode = malloc(sizeof(*newnode));
+	if (!newnode)
+		return (NULL);
+	newnode->n = index;
+	newnode->prev = (*head)->prev;
+	(*head)->prev = newnode;
+	newnode->prev->next = newnode;
+	newnode->next = (*head);
 }
