d1c1cb89fc9de404f9294da32a9b688f59ac0df2
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-28 06:40:45 +0000

    .

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,18 @@
 #include "lists.h"
 /**
+ *sum_dlistint - Added sum of the value in 'n'
  *
+ *@head: The pointer to the list
+ *Return: The full sum of the list value
  */
 int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+  
+	while (head != NULL)
+	{
+	sum += head->n;
+	head = head->next;
+	}
+return (sum);
+}
