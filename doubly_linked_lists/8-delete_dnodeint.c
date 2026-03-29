44377bfe38663c9b4c3b8aa56edbdb65f83ed07f
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-29 05:27:55 +0000

    .

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,5 +4,15 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int i;
+	dlistint_t *cur;
 
+	for (i = 0; i < index && cur != NULL; i++)
+
+	if ( == 1)
+	return()
+	if ( == -1)
+	return ()
+
+return ();
 }
