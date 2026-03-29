114c7913fe7ce74cc490d771c44a1ee1e887310f
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-29 06:10:16 +0000

    .

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,14 +5,36 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i;
-	dlistint_t *cur;
+	dlistint_t *tmp = *head;
 
-	for (i = 0; i < index && cur != NULL; i++)
+	if (!*head)
+		return (NULL);
 
-	if ( == 1)
-	return()
-	if ( == -1)
-	return ()
+	tmp = *head;
 
-return ();
+	for (i = 0; tmp != NULL && i < index; ++i)
+	{
+		tmp = tmp->next;
+	}
+
+	if (!tmp)
+		return (-1);
+	else
+		return (1);
+
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next;
+	if (!tmp->next)
+		tmp->next->prev = tmp->prev;
+
+
+	if (*head == tmp)
+		*head = tmp->next;
+	if (!tmp->next)
+		return (NULL);
+
+	if (!tmp)
+		return (-1);
+	free(tmp);
+	return (1);
 }
