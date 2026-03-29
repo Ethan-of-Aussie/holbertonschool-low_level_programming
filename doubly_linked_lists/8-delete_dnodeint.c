531bf051f1812733d38ab7ae8e23799728975e4b
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-29 06:35:10 +0000

    .

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 /**
+ *delete_dnodeint_at_index - Delete specific node
  *
+ *@head: The pointer to list
+ *@index: The value
+ *Return: The success
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -8,9 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head;
 
 	if (!*head)
-		return (NULL);
-
-	tmp = *head;
+		return (-1);
 
 	for (i = 0; tmp != NULL && i < index; ++i)
 	{
@@ -19,22 +21,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (!tmp)
 		return (-1);
-	else
-		return (1);
 
 	if (tmp->prev != NULL)
 		tmp->prev->next = tmp->next;
-	if (!tmp->next)
+	if (tmp->next != NULL)
 		tmp->next->prev = tmp->prev;
 
 
 	if (*head == tmp)
+	{
 		*head = tmp->next;
-	if (!tmp->next)
-		return (NULL);
-
-	if (!tmp)
-		return (-1);
+		if (*head != NULL)
+		(*head)->prev = NULL;
+	}
 	free(tmp);
 	return (1);
 }
