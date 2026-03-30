a4587ad9d7a9a28caaf1d5cc036e0408694336dc
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-30 09:19:04 +0000

    .

---
+++ file_io/3-cp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, r, w;
+	char buffer[1024];
+
+	if (argc != 3)
+	dprintf{STDERR_FILENO, "Usage: cp file_from file_to\n"}, exit(97);
+
+	
+}
