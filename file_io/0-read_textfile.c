8c1284090db9ac051d67d3c0624fe91418e84c78
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-30 06:38:15 +0000

    .

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,10 +8,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer = malloc(letters);
 	ssize_t r, w;
 
-  if (!filename)
-    return (0);
-  for (i = 0; i < letters; i++)
-    write (1, &i, 1);
-    
-  return ();
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
+
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	w =write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+  return (w);
 }
