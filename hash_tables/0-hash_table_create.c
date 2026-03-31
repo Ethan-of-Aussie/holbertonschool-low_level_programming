96e4713ef74880bd4da8654a2adbc29885c99628
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-31 00:40:05 +0000

    .

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -5,9 +5,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_node_t *newnode = malloc(sizeof(*newnode));
-	hash_table_t *ht;
+	hash_table_t *ht = malloc(sizeof(*ht));
 
 	if (!newnode)
+	  return (NULL);
+	if (!ht)
 	  return (NULL);
 
 	newnode->next =
