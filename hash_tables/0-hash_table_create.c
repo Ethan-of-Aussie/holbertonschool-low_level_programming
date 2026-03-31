d6d43c6d7102d2d12677514cd62742c6b7b9dd6b
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-31 01:47:20 +0000

    .

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,25 +1,23 @@
 #include "hash_tables.h"
 /**
+ *hash_table_create - Creating the hash table
  *
+ *@size: The size of the array
+ *Return: The created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-
 	hash_table_t *ht = malloc(sizeof(*ht));
 	if (!ht)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(*ht->array) * size);
+	ht->array = calloc(size, sizeof(*ht->array));
 	if (!ht->array)
 		{
 		free(ht);
 		return (NULL);
 		}
 
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
-return (NULL);
+return (ht);
 }
