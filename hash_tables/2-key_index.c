cb79ab542ba8dd574ad9844ebf2d0a122a9191f4
“Ethan-of-Aussie” <13068@holbertonstudents.com>
2026-04-02 03:33:51 +0000

    Saving progress

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,10 @@
 #include "hash_table.h"
 /**
+ *key_index - Provides index of a key
  *
+ *@key: Holds the element
+ *@size: The size of the index
+ *Return: Area to be sotred in array of hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
