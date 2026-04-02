03fe240bf867fccb2d9f77d54eca41d6cc5fbee2
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-02 10:16:32 +0000

    betty fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	newnode = malloc(sizeof(hash_node_t));
 	if (!newnode)
-	return 0;
+	return (0);
 	newnode->key = strdup(key);
 	newnode->value = strdup(value);
 	newnode->next = ht->array[index];
